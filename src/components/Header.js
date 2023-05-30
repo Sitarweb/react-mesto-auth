99e8ede75500fdb429e6e090849cce1f1b14ea5b
Simon Tarasenko <semeontarasenko@yandex.ru>
2023-05-30 23:20:06 +0300

    12ПР

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../images/header__logo.svg";
+import logo from "../images/header-logo.svg";
 import { Link } from "react-router-dom";
 
 function Header(props) {
